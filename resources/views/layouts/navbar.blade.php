b49961e9fe95867647346b0fc96356f50772b4d9
felicia-sw <felicia.sword@gmail.com>
2025-10-16 22:28:52 +0700

    home page full but need revision, problem with navigation sticky already written but wont work

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,12 +1,6 @@
-
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="" crossorigin="anonymous">
-	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-
 <div class="container-fluid px-5 py-3">
     <header class="mb-4">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-glass rounded-4 shadow-lg p-3">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-glass rounded-4 shadow-lg p-3 sticky-top z-3">
             <div class="container-fluid">
                 <a class="navbar-brand fs-4 fw-semibold text-white" href="{{ route('home') }}">
                     {{ config('app.name') }}
@@ -20,20 +14,26 @@
 
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto gap-3">
-                         <li class="nav-item">
-                            <a class="nav-link text-white px-3 py-2 rounded-2 fs-2" href="{{ route('home') }}">Home</a>
+                        <li class="nav-item">
+                            <a class="nav-link text-white px-3 py-2 rounded-2 fs-2"
+                                href="{{ route('home') }}#features">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white px-3 py-2 rounded-2 fs-2" href="{{ route('event') }}">Events</a>
+                            <a class="nav-link text-white px-3 py-2 rounded-2 fs-2"
+                                href="{{ route('home') }}#events">Events</a>
                         </li>
                         <li class="nav-item">
-                           <a class="nav-link text-white px-3 py-2 rounded-2 fs-2" href="{{ route('art_gallery') }}">Art Gallery</a>
-                        </li>
-                          <li class="nav-item">
-                            <a class="nav-link text-white px-3 py-2 rounded-2 fs-2" href="{{ route('about') }}">About Us</a>
+                            <a class="nav-link text-white px-3 py-2 rounded-2 fs-2"
+                                href="{{ route('home') }}#gallery">Art Gallery</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white px-3 py-2 rounded-2 fs-2" href="{{ route('contact') }}">Contact</a>
+                            <a class="nav-link text-white px-3 py-2 rounded-2 fs-2"
+                                href="{{ route('home') }}#about">About
+                                Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white px-3 py-2 rounded-2 fs-2"
+                                href="{{ route('home') }}#contact">Contact</a>
                         </li>
                     </ul>
                 </div>
