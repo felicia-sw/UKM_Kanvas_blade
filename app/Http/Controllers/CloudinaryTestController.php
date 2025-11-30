7a700834546677add70f41097444231a35091623
felicia-sw <felicia.sword@gmail.com>
2025-11-30 21:02:53 +0700

    pembuatan traits, tapi blm di apply di controllers lain

--- app/Http/Controllers/CloudinaryTestController.php
+++ app/Http/Controllers/CloudinaryTestController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+use App\Traits\CloudinaryUpload;
 
 class CloudinaryTestController extends Controller
 {
+    use CloudinaryUpload;
+
     public function showUploadForm()
     {
         return view('upload_form');
@@ -18,17 +20,27 @@ class CloudinaryTestController extends Controller
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
-        $uploadedFileUrl = Cloudinary::upload($request->file('image')->getRealPath(), [
-            'transformation' => [
-                'width' => 500,
-                'crop' => 'limit',
-                'quality' => 'auto',
-                'fetch_format' => 'auto'
-            ]
-        ])->getSecurePath();
+        $uploadedFileUrl = $this->uploadToCloudinary($request->file('image'), 'testing-uploads');
+        $file = $request->file('image');
+        $folder = 'testing-uploads';
 
-        return back()
-            ->with('success', 'Image uploaded successfully')
-            ->with('image_url', $uploadedFileUrl);
+        // Example for a standard upload
+        // $uploadedFileUrl = $this->uploadToCloudinary($file, $folder);
+
+        // Example for a proof of payment with compression
+        $options = [
+            'transformation' => [
+                ['quality' => 'auto:low']
+            ]
+        ];
+        $uploadedFileUrl = $this->uploadToCloudinary($file, $folder, $options);
+
+        if ($uploadedFileUrl) {
+            return back()
+                ->with('success', 'Image uploaded successfully')
+                ->with('image_url', $uploadedFileUrl);
+        }
+
+        return back()->with('error', 'Image could not be uploaded.');
     }
 }
