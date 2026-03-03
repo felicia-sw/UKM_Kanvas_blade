dc645bbcf609fef9d41d9a33f20c72af1d4426ec
Eileen <ecynthia01@student.ciputra.ac.id>
2026-03-03 17:11:10 +0700

    update nama tab ukm kanvas
    also remove debugbar

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,22 +3,13 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '37e03c4c2fccf4fc4ddffff510751822a8bd6364',
+        'reference' => '03d47b03cdb09895269f084a11e5c1ae1097bdc0',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'barryvdh/laravel-debugbar' => array(
-            'pretty_version' => 'v3.16.0',
-            'version' => '3.16.0.0',
-            'reference' => 'f265cf5e38577d42311f1a90d619bcd3740bea23',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../barryvdh/laravel-debugbar',
-            'aliases' => array(),
-            'dev_requirement' => true,
-        ),
         'brick/math' => array(
             'pretty_version' => '0.14.0',
             'version' => '0.14.0.0',
@@ -439,7 +430,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '37e03c4c2fccf4fc4ddffff510751822a8bd6364',
+            'reference' => '03d47b03cdb09895269f084a11e5c1ae1097bdc0',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -589,12 +580,6 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
-        'maximebf/debugbar' => array(
-            'dev_requirement' => true,
-            'replaced' => array(
-                0 => 'v2.2.4',
-            ),
-        ),
         'mockery/mockery' => array(
             'pretty_version' => '1.6.12',
             'version' => '1.6.12.0',
@@ -697,15 +682,6 @@
             'reference' => '4f7fd7836c6f332bb2933569e566a0d6c4cbed74',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phar-io/version',
-            'aliases' => array(),
-            'dev_requirement' => true,
-        ),
-        'php-debugbar/php-debugbar' => array(
-            'pretty_version' => 'v2.2.4',
-            'version' => '2.2.4.0',
-            'reference' => '3146d04671f51f69ffec2a4207ac3bdcf13a9f35',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../php-debugbar/php-debugbar',
             'aliases' => array(),
             'dev_requirement' => true,
         ),
